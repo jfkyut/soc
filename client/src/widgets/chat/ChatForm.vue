02d21ba09f5413d25d5b7852167069efca7f25eb
jfkyut <jfarnosa.github@gmail.com>
2024-05-04 15:59:10 +0800

    readonly blob on textarea

--- client/src/widgets/chat/ChatForm.vue
+++ client/src/widgets/chat/ChatForm.vue
@@ -74,6 +74,7 @@ onMounted(() => {
         id="chat" 
         rows="1" 
         @keydown="handleKeyDown"
+        :readonly="typeof form.content === 'object' && form.content !== null"
         ref="messageTextAreaRef"
         class="block mx-4 p-2.5 w-full resize-none text-sm outline-none text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
         placeholder="Your message...">
